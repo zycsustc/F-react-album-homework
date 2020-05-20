f81892ec8579ef7719e761d6acdd20ad706e45cd
杜娟 <jdu.xian@gmail.com>
2020-05-20 15:36:14 +0800

    add fetch albums and photos api

--- src/apiUtil.js
+++ src/apiUtil.js
@@ -4,4 +4,12 @@ function fetchUser() {
   return fetch(`${API}/users/1`).then((data) => data.json());
 }
 
-export { fetchUser };
+function fetchAlbums() {
+  return fetch(`${API}/users/1/albums`).then((data) => data.json());
+}
+
+function fetchPhotos(albumId) {
+  return fetch(`${API}/albums/${albumId}/photos`).then((data) => data.json());
+}
+
+export { fetchUser, fetchAlbums, fetchPhotos };
